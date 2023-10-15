a2a947b2896b2f966baae2db303069407552b2c1
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-10-15 13:46:15 +0300

    Upload

---
+++ CSharp-Advanced/OOP-CSharp-June-2023/09. Mocking and Test Driven Development Lab/HeroTests.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class HeroTests
+    {
+        [Test]
+        public void TestHeroGainsXPWhenTargetDies()
+        {
+            // Arrange
+            Mock<ITarget> targetMock = new Mock<ITarget>();
+            targetMock.Setup(t => t.GiveExperience()).Returns(100);
+            targetMock.Setup(t => t.IsDead()).Returns(true);
+
+            Mock<IWeapon> weaponMock = new Mock<IWeapon>();
+            weaponMock.Setup(w => w.Attack(targetMock.Object));
+
+            // Pass null as the first argument, and a fake weapon as the second argument
+            Hero hero = new Hero(null, weaponMock.Object);
+
+            // Act
+            hero.Attack(targetMock.Object);
+
+            // Assert
+            Assert.AreEqual(100, hero.Experience);
+        }
+    }
+}
+

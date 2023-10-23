93df61c736ae0ab35241fb1c9237e1e6904d7865
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2023-10-23 23:25:09 +0300

    Delete

--- CSharp-DB/EF-Core-October-2023/02. ORM/MiniORM.App/Config.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MiniORM.App
-{
-    public static class Config
-    {
-        public const string ConnectionString =
-            @"Server=.\SQLEXPRESS;Database=MiniORM;Integrated Security=True;Encrypt=False;";
-    }
-}
--- CSharp-DB/EF-Core-October-2023/02. ORM/MiniORM/ExceptionMessages.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MiniORM
-{
-    internal static class ExceptionMessages
-    {
-        public const string EntityNullException = "Entity cannot be null!";
-
-        public const string InvalidEntitiesException = "{0} Invalid Entities found in {1}!";
-    }
-}

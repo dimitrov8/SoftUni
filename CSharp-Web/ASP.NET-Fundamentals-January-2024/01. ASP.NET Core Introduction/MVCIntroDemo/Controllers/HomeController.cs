9d5ed37de838bcd08d4991d259c94ec16f509844
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-14 23:50:23 +0200

    Upload

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/01. ASP.NET Core Introduction/MVCIntroDemo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿namespace MVCIntroDemo.Controllers;
+
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        this.logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        this.ViewBag.Message = "Hello World!";
+        return this.View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return this.View();
+    }
+
+    public IActionResult About()
+    {
+        this.ViewBag.Message = "This is an ASP.NET Core MVC app.";
+        return this.View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+    }
+}

e758bd43fad979f6cb3ce4af00de3baacc6ccef0
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-15 16:20:50 +0200

    Add ProductController with a sample list of products and an action to display all products

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/01. ASP.NET Core Introduction/MVCIntroDemo/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿namespace MVCIntroDemo.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+public class ProductController : Controller
+{
+	private IEnumerable<ProductViewModel> products = new List<ProductViewModel>
+	{
+		new()
+		{
+			Id = 1,
+			Name = "Cheese",
+			Price = 7.00
+		},
+		new()
+		{
+			Id = 2,
+			Name = "Ham",
+			Price = 5.50
+		},
+		new()
+		{
+			Id = 3,
+			Name = "Bread",
+			Price = 1.50
+		}
+	};
+
+	public IActionResult All()
+	{
+		return this.View(this.products);
+	}
+}

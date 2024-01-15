59bd164508b92813d40e6b22837af01424ff8e41
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-15 20:34:52 +0200

    Upload

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/01. ASP.NET Core Introduction/02. ChatApp/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,40 @@
+﻿namespace ChatApp.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+public class ChatController : Controller
+{
+	private static List<KeyValuePair<string, string>> sMessages = new();
+
+	public IActionResult Show()
+	{
+		if (sMessages.Any() == false)
+		{
+			return this.View(new ChatViewModel());
+		}
+
+		var chatModel = new ChatViewModel
+		{
+			Messages = sMessages
+				.Select(m => new MessageViewModel
+				{
+					Sender = m.Key,
+					MessageText = m.Value
+				})
+				.ToList()
+		};
+
+		return this.View(chatModel);
+	}
+
+	[HttpPost]
+	public IActionResult Send(ChatViewModel chat)
+	{
+		var newMessage = chat.CurrentMessage;
+
+		sMessages.Add(new KeyValuePair<string, string>(newMessage.Sender, newMessage.MessageText));
+
+		return this.RedirectToAction("Show");
+	}
+}

056b284988823e60384a8c322e311db070ad81fb
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-18 22:33:53 +0200

    Create PostController.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/03. [Workshop] Forum App/ForumApp/Controllers/PostController.cs
@@ -0,0 +1,30 @@
+﻿namespace ForumApp.Controllers;
+
+using Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models.Post;
+
+public class PostController : Controller
+{
+	private readonly ForumAppDbContext data;
+
+	public PostController(ForumAppDbContext data)
+	{
+		this.data = data;
+	}
+
+	public async Task<IActionResult> All()
+	{
+		List<PostViewModel> posts = await this.data
+			.Posts
+			.Select(p => new PostViewModel
+			{
+				Id = p.Id,
+				Title = p.Title,
+				Content = p.Content
+			}).ToListAsync();
+
+		return this.View(posts);
+	}
+}

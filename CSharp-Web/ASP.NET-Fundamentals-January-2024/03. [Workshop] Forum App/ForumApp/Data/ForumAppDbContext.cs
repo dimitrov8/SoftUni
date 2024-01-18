68901de0ff306370ab11bae99dda736881d4b7f8
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-18 22:01:04 +0200

    Create ForumAppDbContext.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/03. [Workshop] Forum App/ForumApp/Data/ForumAppDbContext.cs
@@ -0,0 +1,55 @@
+﻿namespace ForumApp.Data;
+
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+public class ForumAppDbContext : DbContext
+{
+	public ForumAppDbContext(DbContextOptions<ForumAppDbContext> options)
+		: base(options)
+	{
+		this.Database.Migrate();
+	}
+
+	private Post FirstPost { get; set; }
+
+	private Post SecondPost { get; set; }
+
+	private Post ThirdPost { get; set; }
+
+	public DbSet<Post> Posts { get; init; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		this.SeedPosts();
+		modelBuilder
+			.Entity<Post>()
+			.HasData(this.FirstPost, this.SecondPost, this.ThirdPost);
+
+		base.OnModelCreating(modelBuilder);
+	}
+
+	private void SeedPosts()
+	{
+		this.FirstPost = new Post
+		{
+			Id = 1,
+			Title = "My first post",
+			Content = "My first post will be about performing CRUD operations in MVC. It's so much fun!"
+		};
+
+		this.SecondPost = new Post
+		{
+			Id = 2,
+			Title = "My second post",
+			Content = "This is my second post. CRUD operations in MVC are getting more and more interesting!"
+		};
+
+		this.ThirdPost = new Post
+		{
+			Id = 3,
+			Title = "My third post",
+			Content = "Hello there! I'm getting better and better with the CRUD operations in MVC. Stay tuned!"
+		};
+	}
+}

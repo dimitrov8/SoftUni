3fb8545ea5c4dce502477d63458cecdef4b6dfba
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-18 19:34:40 +0200

    Remove unnecessary code

--- CSharp-Web/ASP.NET-Fundamentals-January-2024/03. [Workshop] Forum App/ForumApp/Controllers/HomeController.cs
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/03. [Workshop] Forum App/ForumApp/Controllers/HomeController.cs
@@ -8,10 +8,6 @@ public class HomeController : Controller
 {
 	private readonly ILogger<HomeController> _logger;
 
-	public HomeController(ILogger<HomeController> logger)
-	{
-		this._logger = logger;
-	}
 
 	public IActionResult Index()
 	{

88502abd415bdb60be3ff9dc0857f4f54a18e1b2
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-18 21:18:17 +0200

    Create Post.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/03. [Workshop] Forum App/ForumApp/Data/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿namespace ForumApp.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using static DataConstants;
+
+public class Post
+{
+	public int Id { get; init; }
+
+	[Required]
+	[MaxLength(POST_TITLE_MAX_LENGTH)]
+	public string Title { get; set; } = null!;
+
+	[Required]
+	[MaxLength(POST_CONTENT_MAX_LENGTH)]
+	public string Content { get; set; } = null!;
+}

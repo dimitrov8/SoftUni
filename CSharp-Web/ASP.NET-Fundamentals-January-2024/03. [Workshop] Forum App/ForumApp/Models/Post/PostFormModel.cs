c592d02abef76912292264f62e40d0850cd7699e
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-20 16:31:55 +0200

    Define PostFormModel for post creation with title and content validation

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/03. [Workshop] Forum App/ForumApp/Models/Post/PostFormModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ForumApp.Models.Post;
+
+using System.ComponentModel.DataAnnotations;
+using static Data.DataConstants;
+
+public class PostFormModel
+{
+	[Required]
+	[StringLength(POST_TITLE_MAX_LENGTH, MinimumLength = POST_CONTENT_MIN_LENGTH)]
+	public string Title { get; set; } = null!;
+
+	[StringLength(POST_CONTENT_MAX_LENGTH, MinimumLength = POST_CONTENT_MIN_LENGTH)]
+	public string Content { get; set; } = null!;
+}

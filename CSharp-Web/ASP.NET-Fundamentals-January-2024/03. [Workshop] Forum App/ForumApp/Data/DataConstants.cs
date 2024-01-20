d669bf07c99f7249564eb550ddc3d31b53982b57
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-20 16:29:27 +0200

    Add min values

--- CSharp-Web/ASP.NET-Fundamentals-January-2024/03. [Workshop] Forum App/ForumApp/Data/DataConstants.cs
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/03. [Workshop] Forum App/ForumApp/Data/DataConstants.cs
@@ -4,5 +4,8 @@ public static class DataConstants
 {
 	// Post
 	public const int POST_TITLE_MAX_LENGTH = 50;
+	public const int POST_TITLE_MIN_LENGTH = 10;
+
 	public const int POST_CONTENT_MAX_LENGTH = 1500;
+	public const int POST_CONTENT_MIN_LENGTH = 10;
 }

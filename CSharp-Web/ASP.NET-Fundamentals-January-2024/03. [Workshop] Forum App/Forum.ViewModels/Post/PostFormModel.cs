b5db172ac5494cf50e397420caf89b7fd8f63f42
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-21 21:58:19 +0200

    Create PostFormModel.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/03. [Workshop] Forum App/Forum.ViewModels/Post/PostFormModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Forum.ViewModels.Post;
+
+using System.ComponentModel.DataAnnotations;
+using static Common.Validations.EntityValidations.Post;
+
+public class PostFormModel
+{
+	[Required]
+	[StringLength(POST_TITLE_MAX_LENGTH, MinimumLength = POST_TITLE_MIN_LENGTH)]
+	public string Title { get; set; } = null!;
+
+	[Required]
+	[StringLength(POST_CONTENT_MAX_LENGTH, MinimumLength = POST_CONTENT_MIN_LENGTH)]
+	public string Content { get; set; } = null!;
+}

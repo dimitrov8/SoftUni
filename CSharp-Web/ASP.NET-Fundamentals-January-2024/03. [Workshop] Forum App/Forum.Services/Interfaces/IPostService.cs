91c93fe6a90813844e70db8dcc74ecc894692c31
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-21 21:58:07 +0200

    Create IPostService.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/03. [Workshop] Forum App/Forum.Services/Interfaces/IPostService.cs
@@ -0,0 +1,16 @@
+﻿namespace Forum.Services.Interfaces;
+
+using ViewModels.Post;
+
+public interface IPostService
+{
+	Task<IEnumerable<PostViewModel>> ListAllAsync();
+
+	Task AddPostAsync(PostFormModel model);
+
+	Task<PostFormModel> GetPostForEditOrDeleteAsync(string id);
+
+	Task EditByIdAsync(string id, PostFormModel model);
+
+	Task DeleteByIdAsync(string id);
+}

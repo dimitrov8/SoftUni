283ceba7c07dabb927d030d47257b5742a5abbb6
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-21 21:57:24 +0200

    Create Post.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/03. [Workshop] Forum App/Forum.Data.Models/Post.cs
@@ -0,0 +1,23 @@
+﻿namespace Forum.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using static Common.Validations.EntityValidations.Post;
+
+public class Post
+{
+	public Post()
+	{
+		this.Id = Guid.NewGuid();
+	}
+
+	[Key]
+	public Guid Id { get; set; }
+
+	[Required]
+	[MaxLength(POST_TITLE_MAX_LENGTH)]
+	public string Title { get; set; } = null!;
+
+	[Required]
+	[MaxLength(POST_CONTENT_MAX_LENGTH)]
+	public string Content { get; set; } = null!;
+}

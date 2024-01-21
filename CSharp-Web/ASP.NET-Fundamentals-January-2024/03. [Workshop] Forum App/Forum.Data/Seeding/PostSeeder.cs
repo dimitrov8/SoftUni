e39446eb7576d418845b4f3524087e75c7a1f617
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-21 21:57:59 +0200

    Create PostSeeder.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/03. [Workshop] Forum App/Forum.Data/Seeding/PostSeeder.cs
@@ -0,0 +1,30 @@
+﻿namespace Forum.Data.Seeding;
+
+using Models;
+
+internal class PostSeeder
+{
+	internal Post[] GeneratePosts()
+	{
+		ICollection<Post> posts = new HashSet<Post>
+		{
+			new()
+			{
+				Title = "My first post",
+				Content = "My first post will be about performing CRUD operations in MVC. It's so much fun!"
+			},
+			new()
+			{
+				Title = "My second post",
+				Content = "This is my second post. CRUD operations in MVC are getting more and more interesting!"
+			},
+			new()
+			{
+				Title = "My third post",
+				Content = "Hello there! I'm getting better and better with the CRUD operations in MVC. Stay tuned!"
+			}
+		};
+
+		return posts.ToArray();
+	}
+}

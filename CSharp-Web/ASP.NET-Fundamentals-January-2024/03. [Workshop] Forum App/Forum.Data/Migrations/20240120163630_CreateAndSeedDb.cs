1c49669a2f38e31b38520bf18f707958906a7f3d
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-21 21:57:46 +0200

    Create 20240120163630_CreateAndSeedDb.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/03. [Workshop] Forum App/Forum.Data/Migrations/20240120163630_CreateAndSeedDb.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ForumApp.Data.Migrations
+{
+    public partial class CreateAndSeedDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(1500)", maxLength: 1500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { new Guid("75ea7579-2f88-487d-ba7a-0059fb3bd3fc"), "This is my second post. CRUD operations in MVC are getting more and more interesting!", "My second post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { new Guid("cce8ff06-0a74-42a3-9d33-4b65698ebc20"), "Hello there! I'm getting better and better with the CRUD operations in MVC. Stay tuned!", "My third post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { new Guid("ec0a6000-befa-41fd-90ee-29d4e6116cac"), "My first post will be about performing CRUD operations in MVC. It's so much fun!", "My first post" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}

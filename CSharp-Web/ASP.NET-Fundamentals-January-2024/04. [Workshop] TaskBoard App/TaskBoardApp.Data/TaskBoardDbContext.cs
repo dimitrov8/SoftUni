404b1d6fceb3a9c9e3430bfa4524fb2b23ff73f2
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-26 14:42:08 +0200

    Add DbContext with Identity and Board/Task entities

--- CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp.Data/TaskBoardDbContext.cs
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp.Data/TaskBoardDbContext.cs
@@ -1,12 +1,25 @@
 ﻿namespace TaskBoardApp.Data;
 
+using System.Reflection;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Models;
 
 public class TaskBoardDbContext : IdentityDbContext
 {
 	public TaskBoardDbContext(DbContextOptions<TaskBoardDbContext> options)
 		: base(options)
 	{
+	}
+
+	public DbSet<Board> Boards { get; set; } = null!;
+
+	public DbSet<Task> Tasks { get; set; } = null!;
+
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		builder.ApplyConfigurationsFromAssembly(Assembly.GetAssembly(typeof(TaskBoardDbContext)) ?? Assembly.GetExecutingAssembly());
+
+		base.OnModelCreating(builder);
 	}
 }

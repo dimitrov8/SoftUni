3218575aedc6c7d6496c876c6f0393209bc6d820
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-26 14:44:27 +0200

    Add Board entity with properties and relationships

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp.Data.Models/Board.cs
@@ -0,0 +1,21 @@
+﻿namespace TaskBoardApp.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using static Common.EntityValidationConstants.Board;
+
+public class Board
+{
+	public Board()
+	{
+		this.Tasks = new HashSet<Task>();
+	}
+
+	[Key]
+	public int Id { get; init; }
+
+	[Required]
+	[MaxLength(MAX_BOARD_NAME)]
+	public string Name { get; init; } = null!;
+
+	public ICollection<Task> Tasks { get; set; }
+}

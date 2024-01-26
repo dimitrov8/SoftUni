43399c152efd0460c895683d8d47eb77d4179c50
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-26 14:40:45 +0200

    Remove comments

--- CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,7 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-#nullable disable
+﻿#nullable disable
 
 namespace TaskBoardApp.Areas.Identity.Pages.Account;
 
@@ -26,23 +23,11 @@ public class RegisterModel : PageModel
 		this._signInManager = signInManager;
 	}
 
-	/// <summary>
-	///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-	///     directly from your code. This API may change or be removed in future releases.
-	/// </summary>
 	[BindProperty]
 	public InputModel Input { get; set; }
 
-	/// <summary>
-	///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-	///     directly from your code. This API may change or be removed in future releases.
-	/// </summary>
 	public string ReturnUrl { get; set; }
 
-	/// <summary>
-	///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-	///     directly from your code. This API may change or be removed in future releases.
-	/// </summary>
 	public class InputModel
 	{
 		[Required]
@@ -50,29 +35,17 @@ public class RegisterModel : PageModel
 		[Display(Name = "Username")]
 		public string Username { get; set; }
 
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
 		[Required]
 		[EmailAddress]
 		[Display(Name = "Email")]
 		public string Email { get; set; }
 
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
 		[Required]
 		[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
 		[DataType(DataType.Password)]
 		[Display(Name = "Password")]
 		public string Password { get; set; }
 
-		/// <summary>
-		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-		///     directly from your code. This API may change or be removed in future releases.
-		/// </summary>
 		[DataType(DataType.Password)]
 		[Display(Name = "Confirm password")]
 		[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]

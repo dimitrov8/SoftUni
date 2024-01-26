47fbd56b5967cb4e123694839157a461a76a6b9e
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-26 14:44:41 +0200

    Add Task entity with properties and relationships

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp.Data.Models/Task.cs
@@ -0,0 +1,38 @@
+﻿namespace TaskBoardApp.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using static Common.EntityValidationConstants.Task;
+
+public class Task
+{
+	public Task()
+	{
+		this.Id = Guid.NewGuid();
+	}
+
+	[Key]
+	public Guid Id { get; init; }
+
+	[Required]
+	[MaxLength(MAX_TITLE_LENGTH)]
+	public string Title { get; set; } = null!;
+
+	[Required]
+	[MaxLength(MAX_DESCRIPTION_LENGTH)]
+	public string Description { get; set; } = null!;
+
+	public DateTime CreatedOn { get; set; }
+
+	[ForeignKey(nameof(Board))]
+	public int BoardId { get; set; }
+
+	public Board? Board { get; set; }
+
+	[Required]
+	[ForeignKey(nameof(Owner))]
+	public string OwnerId { get; set; } = null!;
+
+	public IdentityUser Owner { get; set; } = null!;
+}

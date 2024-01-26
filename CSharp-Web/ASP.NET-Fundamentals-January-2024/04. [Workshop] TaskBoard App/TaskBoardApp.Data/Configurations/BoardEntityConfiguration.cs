2ad95faf817cdeb9ddeaed0b36776d61ca522daf
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-26 14:44:58 +0200

    Configure and seed data for Board entity

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp.Data/Configurations/BoardEntityConfiguration.cs
@@ -0,0 +1,39 @@
+﻿namespace TaskBoardApp.Data.Configurations;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Models;
+
+internal class BoardEntityConfiguration : IEntityTypeConfiguration<Board>
+{
+	public void Configure(EntityTypeBuilder<Board> builder)
+	{
+		ICollection<Board> boards = this.GenerateBoards();
+
+		builder.HasData(boards);
+	}
+
+	private ICollection<Board> GenerateBoards()
+	{
+		ICollection<Board> boards = new HashSet<Board>
+		{
+			new()
+			{
+				Id = 1,
+				Name = "Open"
+			},
+			new()
+			{
+				Id = 2,
+				Name = "In Progress"
+			},
+			new()
+			{
+				Id = 3,
+				Name = "Done"
+			}
+		};
+
+		return boards;
+	}
+}

93d8a0edfaf715263e7d781e3280dcb6542e1527
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-26 14:40:00 +0200

    Update EntityValidationConstants.cs

--- CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp.Common/EntityValidationConstants.cs
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp.Common/EntityValidationConstants.cs
@@ -2,5 +2,18 @@
 
 public static class EntityValidationConstants
 {
-	
+	public static class Task
+	{
+		public const int MIN_TITLE_LENGTH = 5;
+		public const int MAX_TITLE_LENGTH = 70;
+
+		public const int MIN_DESCRIPTION_LENGTH = 10;
+		public const int MAX_DESCRIPTION_LENGTH = 1000;
+	}
+
+	public static class Board
+	{
+		public const int MIN_BOARD_NAME = 3;
+		public const int MAX_BOARD_NAME = 30;
+	}
 }

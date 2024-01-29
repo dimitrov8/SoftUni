87ec7e4b121a4c3ff788d115907bf4b682114ad7
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-30 01:18:11 +0200

    Create BoardViewModel.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp.Web.ViewModels/Board/BoardViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskBoardApp.Web.ViewModels.Board;
+
+using Task;
+
+public class BoardViewModel
+{
+	public int Id { get; init; }
+
+	public string Name { get; init; } = null!;
+
+	public ICollection<TaskViewModel> Tasks { get; set; } = null!;
+}

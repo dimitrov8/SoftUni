e31c562cc50c3443ef9a4f91f5cbd58b0f8ebc42
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-30 01:18:21 +0200

    Create TaskViewModel.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp.Web.ViewModels/Task/TaskViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskBoardApp.Web.ViewModels.Task;
+
+public class TaskViewModel
+{
+	public string Id { get; init; } = null!;
+
+	public string Title { get; init; } = null!;
+
+	public string Description { get; init; } = null!;
+
+	public string Owner { get; set; } = null!;
+}

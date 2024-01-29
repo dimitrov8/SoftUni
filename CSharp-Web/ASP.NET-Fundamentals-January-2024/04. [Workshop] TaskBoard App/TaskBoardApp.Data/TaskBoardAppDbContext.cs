0991f344e263cb7dee0d4a32ec125eaa928a5af1
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-30 01:17:26 +0200

    Create TaskBoardAppDbContext.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp.Data/TaskBoardAppDbContext.cs
@@ -0,0 +1,25 @@
+﻿namespace TaskBoardApp.Data;
+
+using System.Reflection;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+public class TaskBoardAppDbContext : IdentityDbContext
+{
+	public TaskBoardAppDbContext(DbContextOptions<TaskBoardAppDbContext> options)
+		: base(options)
+	{
+	}
+
+	public DbSet<Board> Boards { get; set; } = null!;
+
+	public DbSet<Task> Tasks { get; set; } = null!;
+
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		builder.ApplyConfigurationsFromAssembly(Assembly.GetAssembly(typeof(TaskBoardAppDbContext)) ?? Assembly.GetExecutingAssembly());
+
+		base.OnModelCreating(builder);
+	}
+}

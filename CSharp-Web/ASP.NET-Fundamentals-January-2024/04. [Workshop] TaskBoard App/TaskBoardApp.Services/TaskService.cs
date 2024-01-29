eea7592ea1fdb496c56836528e4a2e677c1f7d71
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-30 01:17:53 +0200

    Create TaskService.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp.Services/TaskService.cs
@@ -0,0 +1,49 @@
+﻿namespace TaskBoardApp.Services;
+
+using Data;
+using Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Web.ViewModels.Task;
+
+public class TaskService : ITaskService
+{
+	private readonly TaskBoardAppDbContext _dbContext;
+
+	public TaskService(TaskBoardAppDbContext dbContext)
+	{
+		this._dbContext = dbContext;
+	}
+
+	public async Task AddAsync(string ownerId, TaskFormModel viewModel)
+	{
+		var task = new Data.Models.Task
+		{
+			Title = viewModel.Title,
+			Description = viewModel.Description,
+			BoardId = viewModel.BoardId,
+			CreatedOn = DateTime.UtcNow,
+			OwnerId = ownerId
+		};
+
+		await this._dbContext.Tasks.AddAsync(task);
+		await this._dbContext.SaveChangesAsync();
+	}
+
+	public async Task<TaskDetailsViewModel> GetDetailsByIdAsync(string id)
+	{
+		var viewModel = await this._dbContext
+			.Tasks
+			.Select(t => new TaskDetailsViewModel
+			{
+				Id = t.Id.ToString(),
+				Title = t.Title,
+				Description = t.Description,
+				Owner = t.Owner.UserName,
+				CreatedOn = t.CreatedOn.ToString("f"),
+				Board = t.Board.Name
+			})
+			.FirstAsync(t => t.Id == id);
+
+		return viewModel;
+	}
+}

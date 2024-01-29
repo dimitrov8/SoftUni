5ac0dbbc70b1f90ce50489d7ddb3a1cbde48790e
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-01-30 01:18:26 +0200

    Create BoardController.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp/Controllers/BoardController.cs
@@ -0,0 +1,23 @@
+﻿namespace TaskBoardApp.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+using Web.ViewModels.Board;
+
+public class BoardController : Controller
+{
+	private readonly IBoardService _boardService;
+
+	public BoardController(IBoardService boardService)
+	{
+		this._boardService = boardService;
+	}
+
+	public async Task<IActionResult> All()
+	{
+		IEnumerable<BoardViewModel> allBoards =
+			await this._boardService.AllAsync();
+
+		return this.View(allBoards);
+	}
+}

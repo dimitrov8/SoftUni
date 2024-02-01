e9c63015504b6976e0fcd6ea36870dbec6bb4e10
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-01 23:41:32 +0200

    Update ITaskService.cs

--- CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp.Services/Interfaces/ITaskService.cs
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp.Services/Interfaces/ITaskService.cs
@@ -8,4 +8,8 @@ public interface ITaskService
 	Task AddAsync(string ownerId, TaskFormModel viewModel);
 
 	Task<TaskDetailsViewModel> GetDetailsByIdAsync(string id);
+
+	Task<TaskFormModel?> GetForEditAsync(string id);
+
+	Task EditAsync(string id, TaskFormModel model);
 }

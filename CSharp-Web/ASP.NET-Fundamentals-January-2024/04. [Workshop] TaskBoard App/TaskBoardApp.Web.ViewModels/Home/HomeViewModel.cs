6602d6857d867e337157f70ad925c9c036e13fe2
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-02 23:08:45 +0200

    Create HomeViewModel.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp.Web.ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskBoardApp.Web.ViewModels.Home;
+
+public class HomeViewModel
+{
+	public int AllTasksCount { get; set; }
+
+	public IEnumerable<HomeBoardModel> BoardsWithTasksCount { get; set; } = new List<HomeBoardModel>();
+
+	public int UserTasksCount { get; set; }
+}

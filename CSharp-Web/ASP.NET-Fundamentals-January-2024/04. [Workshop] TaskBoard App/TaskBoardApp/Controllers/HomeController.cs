8fb965c789ff2b0b1168191508972da9370dfd73
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-02 23:08:59 +0200

    Update HomeController.cs

--- CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp/Controllers/HomeController.cs
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/04. [Workshop] TaskBoard App/TaskBoardApp/Controllers/HomeController.cs
@@ -1,11 +1,32 @@
 ﻿namespace TaskBoardApp.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+using Web.ViewModels.Home;
 
 public class HomeController : Controller
 {
-	public IActionResult Index()
+	private readonly ITaskService _taskService;
+	private readonly IBoardService _boardService;
+
+	public HomeController(ITaskService taskService, IBoardService boardService)
 	{
-		return this.View();
+		this._taskService = taskService;
+		this._boardService = boardService;
+	}
+
+	public async Task<IActionResult> Index()
+	{
+		IEnumerable<HomeBoardModel> boardsWithTasksCount = await this._boardService.GetBoardsWithTasksCountAsync();
+		int userTaskCount = await this._taskService.GetUserTasksCountAsync(this.User);
+
+		var homeModel = new HomeViewModel
+		{
+			AllTasksCount = await this._taskService.GetAllTasksCountAsync(),
+			BoardsWithTasksCount = boardsWithTasksCount,
+			UserTasksCount = userTaskCount
+		};
+
+		return this.View(homeModel);
 	}
 }

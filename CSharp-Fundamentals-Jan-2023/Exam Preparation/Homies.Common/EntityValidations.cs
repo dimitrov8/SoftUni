1526c2f5b0e47fca70ddc246f2fa15c2fd76edd2
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-05 00:46:36 +0200

    Create EntityValidations.cs

---
+++ CSharp-Fundamentals-Jan-2023/Exam Preparation/Homies.Common/EntityValidations.cs
@@ -0,0 +1,19 @@
+﻿namespace Homies.Common;
+
+public static class EntityValidations
+{
+    public static class Event
+    {
+        public const int NAME_MIN_LENGTH = 5;
+        public const int NAME_MAX_LENGTH = 20;
+
+        public const int DESCRIPTION_MIN_LENGTH = 15;
+        public const int DESCRIPTION_MAX_LENGTH = 150;
+    }
+
+    public static class Type
+    {
+        public const int NAME_MIN_LENGTH = 5;
+        public const int NAME_MAX_LENGTH = 15;
+    }
+}

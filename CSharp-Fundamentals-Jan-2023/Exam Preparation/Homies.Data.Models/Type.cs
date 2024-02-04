a4c75caece1c37432245c44488eb4fd99b30bf6c
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-05 00:46:05 +0200

    Create Type.cs

---
+++ CSharp-Fundamentals-Jan-2023/Exam Preparation/Homies.Data.Models/Type.cs
@@ -0,0 +1,21 @@
+﻿namespace Homies.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using static Homies.Common.EntityValidations.Type;
+
+public class Type
+{
+	public Type()
+	{
+		this.Events = new HashSet<Event>();
+	}
+
+	[Key]
+	public int Id { get; init; }
+
+	[Required]
+	[MaxLength(NAME_MAX_LENGTH)]
+	public string Name { get; init; } = null!;
+
+	public ICollection<Event> Events { get; set; }
+}

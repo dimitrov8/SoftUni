934e34008af738d9d22640077d349ef9b6f4a4eb
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-09 10:29:52 +0200

    mplement HomeController with Index and Error Handling

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/Homies/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿namespace Homies.Controllers;
+
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+public class HomeController : BaseController
+{
+	[AllowAnonymous]
+	public IActionResult Index()
+	{
+		if (this.User.Identity.IsAuthenticated)
+		{
+			return this.RedirectToAction("All", "Event");
+		}
+
+		return this.View("Index");
+	}
+
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+	}
+}

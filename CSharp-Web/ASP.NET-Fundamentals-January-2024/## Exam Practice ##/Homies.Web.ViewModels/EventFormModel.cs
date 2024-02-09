1cf43d74f26ce0e32d7d98806f073174e1222a14
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-09 10:28:18 +0200

    Create EventFormModel.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/Homies.Web.ViewModels/EventFormModel.cs
@@ -0,0 +1,31 @@
+﻿namespace Homies.Web.ViewModels;
+
+using System.ComponentModel.DataAnnotations;
+using static Common.EntityValidations.Event;
+
+public class EventFormModel
+{
+	[Required]
+	[StringLength(NAME_MAX_LENGTH, MinimumLength = NAME_MIN_LENGTH)]
+	public string Name { get; set; } = null!;
+
+	[Required]
+	[StringLength(DESCRIPTION_MAX_LENGTH, MinimumLength = DESCRIPTION_MIN_LENGTH)]
+	public string Description { get; set; } = null!;
+
+	[Required]
+	[Display(Name = "Start")]
+	public string Start { get; set; } = DateTime.Now.ToString("dd-MM-yyyy H:mm");
+
+	[Required]
+	[Display(Name = "End")]
+	public string End { get; set; } = DateTime.Now.ToString("dd-MM-yyyy H:mm");
+
+	[Required]
+	[Display(Name = "Type of event")]
+	public int TypeId { get; set; }
+
+	public IEnumerable<EventTypeSelectViewModel>? Types { get; set; }
+
+	public string OrganaiserId { get; set; } = null!;
+}

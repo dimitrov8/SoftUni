025e42f9144180aca6b1c80d90761708b728274e
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-09 10:28:21 +0200

    Create EventJoinedViewModel.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/Homies.Web.ViewModels/EventJoinedViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Homies.Web.ViewModels;
+
+public class EventJoinedViewModel
+{
+	public int Id { get; set; }
+
+	public string Name { get; set; } = null!;
+
+	public string Start { get; set; } = null!;
+
+	public string Type { get; set; } = null!;
+
+	public string Organiser { get; set; } = null!;
+
+	public string UserId { get; set; } = null!;
+}

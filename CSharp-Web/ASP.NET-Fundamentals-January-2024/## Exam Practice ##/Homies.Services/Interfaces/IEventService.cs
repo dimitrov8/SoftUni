74912210263acc59714dd6d6886d5537e6667576
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-09 10:27:56 +0200

    Define EventService Interface

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/Homies.Services/Interfaces/IEventService.cs
@@ -0,0 +1,24 @@
+﻿namespace Homies.Services.Interfaces;
+
+using Web.ViewModels;
+
+public interface IEventService
+{
+	Task<IEnumerable<EventTypeSelectViewModel>> GetTypesAsync();
+
+	Task<bool> AddAsync(string organiserId, EventFormModel model);
+
+	Task<IEnumerable<EventAllViewModel>> GetAllAsync();
+
+	Task<EventDetailsViewModel?> ViewDetailsAsync(int id);
+
+	Task<EventFormModel?> GetForEditAsync(int eventId);
+
+	Task EditAsync(int id, EventFormModel model);
+
+	Task<IEnumerable<EventJoinedViewModel>> JoinedAsync(string organiserId);
+
+	Task<bool> JoinAsync(int eventId, string userId);
+
+	Task<bool> LeaveAsync(int eventId, string userId);
+}

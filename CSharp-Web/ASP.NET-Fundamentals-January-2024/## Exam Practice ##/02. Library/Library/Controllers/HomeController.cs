9ac20b726080bfe612336cc7db75b3fd403df752
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-12 15:02:00 +0200

    Create HomeController.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/02. Library/Library/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿namespace Library.Controllers;
+
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+public class HomeController : Controller
+{
+	[AllowAnonymous]
+	public IActionResult Index()
+	{
+		return this.View();
+	}
+
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+	}
+}

245b1a73349634e19ea05927ed13c3ddbb9e2dda
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-12 15:01:39 +0200

    Added common entity validations for Book and Category entities

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/02. Library/Library/Common/EntityValidations.cs
@@ -0,0 +1,28 @@
+﻿namespace Library.Common;
+
+public static class EntityValidations
+{
+	public static class Book
+	{
+		public const int MIN_TITLE_LENGTH = 10;
+		public const int MAX_TITLE_LENGTH = 50;
+
+		public const int MIN_AUTHOR_LENGTH = 5;
+		public const int MAX_AUTHOR_LENGTH = 50;
+
+		public const int MIN_DESCRIPTION_LENGTH = 5;
+		public const int MAX_DESCRIPTION_LENGTH = 5000;
+
+		public const int MIN_RATING_VALUE = 0;
+		public const int MAX_RATING_VALUE = 10;
+	}
+
+	public static class Category
+	{
+		public const int MIN_NAME_LENGTH = 5;
+		public const int MAX_NAME_LENGTH = 50;
+
+		public const int MIN_CATEGORY_VALUE = 1;
+		public const int MAX_CATEGORY_VALUE = int.MaxValue;
+	}
+}

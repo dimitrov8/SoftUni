d164f118fa7e1f63d82f24fd161ae928695c12a2
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-12 15:00:23 +0200

    Defined IBookService interface for book-related operations

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/02. Library/Library/Services/Interfaces/IBookService.cs
@@ -0,0 +1,14 @@
+﻿namespace Library.Services.Interfaces;
+
+using Models;
+
+public interface IBookService
+{
+	Task<IEnumerable<BooksAllViewModel>> AllBooksAsync();
+
+	Task<bool> AddBookToCollectionAsync(string userId, int id);
+
+	Task<IEnumerable<CategoryViewModel>> GetCategoriesAsync();
+
+	Task<bool> AddAsync(string userId, BookAddFormModel model);
+}

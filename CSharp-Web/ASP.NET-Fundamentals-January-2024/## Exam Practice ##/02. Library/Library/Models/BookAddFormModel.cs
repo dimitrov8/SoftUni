bfb09dace3cf117f7d3041bd69cf3654d10fedfb
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-12 14:59:37 +0200

    Implemented BookAddFormModel with validation attributes for book addition form

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/02. Library/Library/Models/BookAddFormModel.cs
@@ -0,0 +1,38 @@
+﻿namespace Library.Models;
+
+using System.ComponentModel.DataAnnotations;
+using static Common.EntityValidations.Book;
+using static Common.EntityValidations.Category;
+
+public class BookAddFormModel
+{
+	[Required]
+	[StringLength(MAX_TITLE_LENGTH)]
+	[MinLength(MIN_TITLE_LENGTH)]
+	public string Title { get; set; } = null!;
+
+	[Required]
+	[StringLength(MAX_AUTHOR_LENGTH)]
+	[MinLength(MIN_AUTHOR_LENGTH)]
+	public string Author { get; set; } = null!;
+
+	[Required]
+	[StringLength(MAX_DESCRIPTION_LENGTH)]
+	[MinLength(MIN_DESCRIPTION_LENGTH)]
+	public string Description { get; set; } = null!;
+
+	[Required]
+	[Url(ErrorMessage = "Invalid URL format")]
+	public string Url { get; set; } = null!;
+
+	[Required]
+	[StringLength(MAX_RATING_VALUE)]
+	[MinLength(MIN_RATING_VALUE)]
+	public string Rating { get; set; } = null!;
+
+	[Required]
+	[Range(MIN_CATEGORY_VALUE, MAX_CATEGORY_VALUE)]
+	public int CategoryId { get; set; }
+
+	public IEnumerable<CategoryViewModel> Categories { get; set; } = new HashSet<CategoryViewModel>();
+}

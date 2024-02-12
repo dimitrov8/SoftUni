e0a04e3d19fdcd6f6f6f1c613bc5054a89facb8a
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-12 14:57:59 +0200

    Added Category entity model

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/02. Library/Library/Data/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿namespace Library.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using static Common.EntityValidations.Category;
+
+public class Category
+{
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	[MaxLength(MAX_NAME_LENGTH)]
+	public string Name { get; set; } = null!;
+
+	public ICollection<Book> Books = new HashSet<Book>();
+}

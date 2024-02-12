770e8facacc81cfb666bb1a248368afa6252c4e4
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-12 14:58:11 +0200

    Added IdentityUserBook entity model

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/02. Library/Library/Data/Models/IdentityUserBook.cs
@@ -0,0 +1,20 @@
+﻿namespace Library.Data.Models;
+
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Framework;
+
+public class IdentityUserBook
+{
+	[Required]
+	public string CollectorId { get; set; } = null!;
+
+	[ForeignKey(nameof(CollectorId))]
+	public IdentityUser Collector { get; set; } = null!;
+
+	[Required]
+	public int BookId { get; set; }
+
+	[ForeignKey(nameof(BookId))]
+	public Book Book { get; set; } = null!;
+}

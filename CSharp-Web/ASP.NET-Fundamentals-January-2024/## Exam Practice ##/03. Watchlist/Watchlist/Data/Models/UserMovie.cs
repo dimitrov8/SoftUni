91c5f314831cb8537f3dc801c62ba1006c91c9f2
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-17 21:30:53 +0200

    Define properties for UserId and MovieId in UserMovie

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/03. Watchlist/Watchlist/Data/Models/UserMovie.cs
@@ -0,0 +1,19 @@
+﻿namespace Watchlist.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class UserMovie
+{
+	[Required]
+	public string UserId { get; set; } = null!;
+
+	[ForeignKey(nameof(UserId))]
+	public User User { get; set; } = null!;
+
+	[Required]
+	public int MovieId { get; set; }
+
+	[ForeignKey(nameof(MovieId))]
+	public Movie Movie { get; set; } = null!;
+}

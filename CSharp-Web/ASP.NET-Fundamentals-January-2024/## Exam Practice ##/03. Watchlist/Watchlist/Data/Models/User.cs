17209adade370f88c55478607c1efa8ebc224508
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-17 21:30:38 +0200

    Create User model inheriting from IdentityUser

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/03. Watchlist/Watchlist/Data/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Watchlist.Data.Models;
+
+using Microsoft.AspNetCore.Identity;
+
+public class User : IdentityUser
+{
+	public ICollection<UserMovie> UsersMovies { get; set; } = new HashSet<UserMovie>();
+}

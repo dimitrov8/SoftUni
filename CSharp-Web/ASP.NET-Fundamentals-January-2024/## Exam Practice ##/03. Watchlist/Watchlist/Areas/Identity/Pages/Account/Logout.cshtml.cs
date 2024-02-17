8d68d5f156d19da0b10e563c89123253f2d9cacf
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-17 21:27:20 +0200

    Add LogoutModel class

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/03. Watchlist/Watchlist/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,30 @@
+﻿#nullable disable
+
+namespace Watchlist.Areas.Identity.Pages.Account;
+
+using Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class LogoutModel : PageModel
+{
+	private readonly SignInManager<User> _signInManager;
+
+	public LogoutModel(SignInManager<User> signInManager)
+	{
+		this._signInManager = signInManager;
+	}
+
+	public async Task<IActionResult> OnPost(string returnUrl = null)
+	{
+		await this._signInManager.SignOutAsync();
+
+		if (returnUrl != null)
+		{
+			return this.LocalRedirect(returnUrl);
+		}
+
+		return this.RedirectToPage();
+	}
+}

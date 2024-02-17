cb5fb0f20ddd2553e0b35827d84024f5acc10e94
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-17 21:30:03 +0200

    Define properties for id and name in Genre

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/03. Watchlist/Watchlist/Data/Models/Genre.cs
@@ -0,0 +1,16 @@
+﻿namespace Watchlist.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using static Common.EntityValidations.Genre;
+
+public class Genre
+{
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	[MaxLength(MAX_NAME_LENGTH)]
+	public string Name { get; set; } = null!;
+
+	public ICollection<Movie> Movies { get; set; } = new HashSet<Movie>();
+}

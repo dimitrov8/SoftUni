f99d5583da83cd65283a00df5b00f6d9c6dedd32
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-17 21:28:31 +0200

    Handle redirection for authenticated users

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/03. Watchlist/Watchlist/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿namespace Watchlist.Controllers;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : BaseController
+{
+	[AllowAnonymous]
+	public IActionResult Index()
+	{
+		if (this.User.Identity?.IsAuthenticated ?? false)
+		{
+			return this.RedirectToAction("All", "Movie");
+		}
+
+		return this.View();
+	}
+}

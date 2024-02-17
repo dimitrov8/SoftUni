9c34c76e3b5e8429778ddd7fde1386ff6ef0fd6b
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-17 21:30:23 +0200

    Define properties for id, title, director, image URL, rating, and genre in Movie

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/03. Watchlist/Watchlist/Data/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿namespace Watchlist.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static Common.EntityValidations.Movie;
+
+public class Movie
+{
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	[MaxLength(MAX_TITLE_LENGTH)]
+	public string Title { get; set; } = null!;
+
+	[Required]
+	[MaxLength(MAX_DIRECTOR_LENGTH)]
+	public string Director { get; set; } = null!;
+
+	[Required]
+	public string ImageUrl { get; set; } = null!;
+
+	[Required]
+	[Range(MIN_RATING_VALUE, MAX_RATING_VALUE)]
+	public decimal Rating { get; set; }
+
+	[Required]
+	public int GenreId { get; set; }
+
+	[Required]
+	[ForeignKey(nameof(GenreId))]
+	public Genre Genre { get; set; } = null!;
+
+	public ICollection<UserMovie> UserMovies { get; set; } = new HashSet<UserMovie>();
+}

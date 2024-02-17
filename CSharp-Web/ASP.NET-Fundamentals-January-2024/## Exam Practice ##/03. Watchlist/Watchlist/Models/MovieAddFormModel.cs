457acb3e88fd29e6d268375022a734b4bdfd9387
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-17 21:32:31 +0200

    Create MovieAddFormModel for movie addition

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/03. Watchlist/Watchlist/Models/MovieAddFormModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Watchlist.Models;
+
+using System.ComponentModel.DataAnnotations;
+using static Common.EntityValidations.Movie;
+
+public class MovieAddFormModel
+{
+	[Required]
+	[StringLength(MAX_TITLE_LENGTH)]
+	[MinLength(MIN_TITLE_LENGTH)]
+	public string Title { get; set; } = null!;
+
+	[Required]
+	[StringLength(MAX_DIRECTOR_LENGTH)]
+	[MinLength(MIN_DIRECTOR_LENGTH)]
+	public string Director { get; set; } = null!;
+
+	[Required]
+	[Url]
+	public string ImageUrl { get; set; } = null!;
+
+	[Required]
+	[Range(MIN_RATING_VALUE, MAX_RATING_VALUE)]
+	public decimal Rating { get; set; }
+
+	public int GenreId { get; set; }
+
+	public IEnumerable<GenreViewModel> Genres { get; set; } = null!;
+}

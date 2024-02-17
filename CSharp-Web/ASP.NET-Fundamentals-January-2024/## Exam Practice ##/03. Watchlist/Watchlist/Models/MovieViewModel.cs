938b7d9ef22577ddd2a24c1300bf5d689690e266
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-17 21:32:45 +0200

    Add MovieViewModel class

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/03. Watchlist/Watchlist/Models/MovieViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Watchlist.Models;
+
+public class MovieViewModel
+{
+	public int Id { get; set; }
+	public string ImageUrl { get; set; } = null!;
+
+	public string Title { get; set; } = null!;
+
+	public string Director { get; set; } = null!;
+
+	public string Rating { get; set; } = null!;
+
+	public string Genre { get; set; } = null!;
+}

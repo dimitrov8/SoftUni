25cb9d201e07f3cc0716f0126b3b8763b9dfac74
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-17 21:38:29 +0200

    Create EntityValidations.cs

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/03. Watchlist/Watchlist/Common/EntityValidations.cs
@@ -0,0 +1,22 @@
+﻿namespace Watchlist.Common;
+
+public static class EntityValidations
+{
+	public static class Movie
+	{
+		public const int MIN_TITLE_LENGTH = 10;
+		public const int MAX_TITLE_LENGTH = 50;
+
+		public const int MIN_DIRECTOR_LENGTH = 5;
+		public const int MAX_DIRECTOR_LENGTH = 50;
+
+		public const int MIN_RATING_VALUE = 0;
+		public const int MAX_RATING_VALUE = 10;
+	}
+
+	public static class Genre
+	{
+		public const int MIN_NAME_LENGTH = 5;
+		public const int MAX_NAME_LENGTH = 50;
+	}
+}

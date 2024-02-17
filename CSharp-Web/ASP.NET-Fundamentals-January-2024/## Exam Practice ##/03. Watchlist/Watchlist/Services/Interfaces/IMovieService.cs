7c34cb8dee2804b4999a19eeec3e8403f9780adb
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-17 21:33:25 +0200

    Add IMovieService interface
    Define methods for retrieving genres, adding movies, retrieving all movies, adding movies to user's collection, removing movies from user's collection, and retrieving watched movies

---
+++ CSharp-Web/ASP.NET-Fundamentals-January-2024/## Exam Practice ##/03. Watchlist/Watchlist/Services/Interfaces/IMovieService.cs
@@ -0,0 +1,18 @@
+﻿namespace Watchlist.Services.Interfaces;
+
+using Models;
+
+public interface IMovieService
+{
+	Task<IEnumerable<GenreViewModel>> GetGenresAsync();
+
+	Task<bool> AddAsync(MovieAddFormModel model);
+
+	Task<IEnumerable<MovieViewModel>> AllAsync();
+
+	Task<bool> AddToCollectionAsync(string userId, int movieId);
+
+	Task<bool> RemoveFromCollectionAsync(string userId, int movieId);
+
+	Task<IEnumerable<MovieViewModel>> GetWatchedAsync(string userId);
+}

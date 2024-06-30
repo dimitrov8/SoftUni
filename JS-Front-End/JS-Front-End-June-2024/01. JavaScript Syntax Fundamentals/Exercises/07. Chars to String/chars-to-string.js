5f3d01161e02cd09fbba177d27335d0e642e2898
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-06-30 05:18:30 +0300

    Upload

---
+++ JS-Front-End/JS-Front-End-June-2024/01. JavaScript Syntax Fundamentals/Exercises/07. Chars to String/chars-to-string.js
@@ -0,0 +1,7 @@
+function solve(...args) {
+  let result = args.join('');
+
+  console.log(result);
+}
+
+solve('a', 'b', 'c');
---
+++ JS-Front-End/JS-Front-End-June-2024/01. JavaScript Syntax Fundamentals/Exercises/08. Reversed Chars/reversed-chars.js
@@ -0,0 +1,7 @@
+function solve(...args) {
+  let result = args.reverse().join(' ');
+
+  console.log(result);
+}
+
+solve('a', 'b', 'c');

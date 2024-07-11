4654a99b16281c5ac09518ed147c56a9c6dda73e
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-07-11 16:50:02 +0300

    Upload

--- JS-Front-End/JS-Front-End-June-2024/03. Functions and Statements/Lab/04. Orders/orders.js
+++ JS-Front-End/JS-Front-End-June-2024/03. Functions and Statements/Lab/04. Orders/orders.js
@@ -1,1 +1,17 @@
-function name(params) {}
+function getTotalPrice(product, quantity) {
+  let price;
+  if (product === 'coffee') {
+    price = 1.5;
+  } else if (product === 'water') {
+    price = 1.0;
+  } else if (product === 'coke') {
+    price = 1.4;
+  } else if (product === 'snacks') {
+    price = 2.0;
+  }
+
+  let totalPrice = (price * quantity).toFixed(2);
+  console.log(totalPrice);
+}
+
+getTotalPrice('coffee', 2);

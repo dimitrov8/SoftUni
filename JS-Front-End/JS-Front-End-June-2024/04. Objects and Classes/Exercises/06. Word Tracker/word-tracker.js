edbadc7e477ebe9237a4ca2cb7bb91e44826210f
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-07-23 00:19:07 +0300

    Upload

---
+++ JS-Front-End/JS-Front-End-June-2024/04. Objects and Classes/Exercises/06. Word Tracker/word-tracker.js
@@ -0,0 +1,37 @@
+function wordsTracker(inputArray) {
+  const wantedWords = inputArray.shift().split(' ');
+  let wantedWordsCounts = {};
+  wantedWords.forEach((word) => (wantedWordsCounts[word] = 0));
+
+  inputArray.forEach(
+    (word) => word in wantedWordsCounts && wantedWordsCounts[word]++
+  );
+  wantedWords.sort((a, b) => wantedWordsCounts[b] - wantedWordsCounts[a]);
+  wantedWords.forEach((word) =>
+    console.log(`${word} - ${wantedWordsCounts[word]}`)
+  );
+}
+
+wordsTracker([
+  'this sentence',
+  'In',
+  'this',
+  'sentence',
+  'you',
+  'have',
+  'to',
+  'count',
+  'the',
+  'occurrences',
+  'of',
+  'the',
+  'words',
+  'this',
+  'and',
+  'sentence',
+  'because',
+  'this',
+  'is',
+  'your',
+  'task',
+]);

c9ddefa32b0504af206cd031f40c8171fe1d8026
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-07-29 06:08:40 +0300

    Upload

--- JS-Front-End/JS-Front-End-June-2024/05. DOM Introduction/Lab/06. Sum Table/SumTable.js
+++ JS-Front-End/JS-Front-End-June-2024/05. DOM Introduction/Lab/06. Sum Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
+  document.getElementById('sum').textContent = '';
+  const total = Array.from(
+    document.querySelectorAll('table tr td:nth-child(even):not(#id)')
+  )
+    .reduce((sum, td) => sum + Number(td.textContent), 0)
+    .toFixed(2);
 
+  document.getElementById('sum').textContent = total;
 }

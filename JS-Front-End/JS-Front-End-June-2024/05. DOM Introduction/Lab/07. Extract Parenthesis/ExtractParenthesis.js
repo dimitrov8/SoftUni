8118e2e591b1752c176819dc03208808f7e595b1
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-07-29 06:38:38 +0300

    Upload

--- JS-Front-End/JS-Front-End-June-2024/05. DOM Introduction/Lab/07. Extract Parenthesis/ExtractParenthesis.js
+++ JS-Front-End/JS-Front-End-June-2024/05. DOM Introduction/Lab/07. Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,12 @@
 function extract(content) {
+  const element = document.getElementById(content);
+  const pattern = /\(([^)]+)\)/g;
 
+  const matches = element.textContent.match(pattern);
+
+  if (matches) {
+    return matches.map((match) => match.slice(1, -1)).join('; ');
+  } else {
+    return 'No matches found.';
+  }
 }

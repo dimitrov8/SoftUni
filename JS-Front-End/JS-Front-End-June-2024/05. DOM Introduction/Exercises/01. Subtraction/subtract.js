910ce7a6e06e1019538c9daa6854df968a503c14
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-07-29 06:46:13 +0300

    Upload

--- JS-Front-End/JS-Front-End-June-2024/05. DOM Introduction/Exercises/01. Subtraction/subtract.js
+++ JS-Front-End/JS-Front-End-June-2024/05. DOM Introduction/Exercises/01. Subtraction/subtract.js
@@ -1,3 +1,6 @@
 function subtract() {
-    console.log('TODO:...');
+  const num1Value = Number(document.getElementById('firstNumber').value);
+  const num2Value = Number(document.getElementById('secondNumber').value);
+
+  document.getElementById('result').textContent = num1Value - num2Value;
 }

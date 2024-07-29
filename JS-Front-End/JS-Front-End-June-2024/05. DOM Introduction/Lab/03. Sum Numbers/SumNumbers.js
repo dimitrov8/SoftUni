0c1cbdb97268268527b58461ea1deb5e4f8427da
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-07-29 05:07:16 +0300

    Upload

--- JS-Front-End/JS-Front-End-June-2024/05. DOM Introduction/Lab/03. Sum Numbers/SumNumbers.js
+++ JS-Front-End/JS-Front-End-June-2024/05. DOM Introduction/Lab/03. Sum Numbers/SumNumbers.js
@@ -1,3 +1,9 @@
 function calc() {
-    // TODO: sum = num1 + num2
+  const num1ElValue = Number(document.getElementById('num1').value);
+  const num2ElValue = Number(document.getElementById('num2').value);
+  if (!isNaN(num1ElValue) && !isNaN(num2ElValue)) {
+    document.getElementById('sum').value = num1ElValue + num2ElValue;
+  } else {
+    document.getElementById('sum').value = 'Invalid input';
+  }
 }

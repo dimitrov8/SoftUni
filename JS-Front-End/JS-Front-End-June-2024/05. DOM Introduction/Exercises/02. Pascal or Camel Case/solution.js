2df8cd0d6631010b0ede4a08e61b8cdaf6141185
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-07-30 22:04:12 +0300

    Uplaod

--- JS-Front-End/JS-Front-End-June-2024/05. DOM Introduction/Exercises/02. Pascal or Camel Case/solution.js
+++ JS-Front-End/JS-Front-End-June-2024/05. DOM Introduction/Exercises/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-  //TODO...
+  const textParts = document
+    .getElementById('text')
+    .value.toLowerCase()
+    .split(' ');
+  const namingConvention = document.getElementById('naming-convention').value;
+  const resultElement = document.getElementById('result');
+  let result;
+
+  if (namingConvention == 'Pascal Case') {
+    result = textParts
+      .map((part) => part[0].toUpperCase() + part.slice(1))
+      .join('');
+  } else if (namingConvention == 'Camel Case') {
+    result = textParts
+      .map((part, index) =>
+        index === 0 ? part : part[0].toUpperCase() + part.slice(1)
+      )
+      .join('');
+  } else {
+    result = 'Error!';
+  }
+
+  resultElement.textContent = result;
 }

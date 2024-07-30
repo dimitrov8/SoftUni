c30656112243f1242fa5191c5a5550159e6cdb2e
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-07-30 23:51:23 +0300

    Upload

--- JS-Front-End/JS-Front-End-June-2024/05. DOM Introduction/Exercises/03. Accordion/accordion.js
+++ JS-Front-End/JS-Front-End-June-2024/05. DOM Introduction/Exercises/03. Accordion/accordion.js
@@ -1,3 +1,10 @@
 function toggle() {
-    console.log('TODO:...');
+  const buttonElement = document.querySelector('span.button');
+  const extraElement = document.getElementById('extra');
+
+  buttonElement.textContent =
+    buttonElement.textContent === 'More' ? 'Less' : 'More';
+
+  extraElement.style.display =
+    buttonElement.textContent === 'More' ? 'none' : 'block';
 }

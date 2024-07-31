163eaffe0b12a15278be8f3a8dc5fc98cbf285cf
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-07-31 23:18:49 +0300

    Upload

--- JS-Front-End/JS-Front-End-June-2024/05. DOM Introduction/Exercises/06. Format the Text/solution.js
+++ JS-Front-End/JS-Front-End-June-2024/05. DOM Introduction/Exercises/06. Format the Text/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-  //TODO
+  const textAreaInputElement = document.getElementById('input');
+  const text = textAreaInputElement.value;
+
+  const sentences = text
+    .split('.')
+    .filter((sentence) => sentence.trim().length > 0)
+    .map((sentance) => sentance.trim() + '.');
+
+  const outputDiv = document.getElementById('output');
+
+  while (outputDiv.firstChild) {
+    outputDiv.removeChild(outputDiv.firstChild);
+  }
+
+  const paragraphCount = Math.ceil(sentences.length / 3);
+
+  for (let i = 0; i < paragraphCount; i++) {
+    const start = i * 3;
+    const end = start + 3;
+    const paragraphSentance = sentences.slice(start, end).join('.');
+
+    const paragraph = document.createElement('p');
+    paragraph.textContent = paragraphSentance;
+
+    outputDiv.appendChild(paragraph);
+  }
 }

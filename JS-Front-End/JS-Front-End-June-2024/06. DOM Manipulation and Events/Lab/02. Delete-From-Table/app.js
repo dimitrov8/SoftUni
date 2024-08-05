c39b6b0869066d3ed051e3489c9ada76be57f05f
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-08-05 05:43:19 +0300

    Upload

--- JS-Front-End/JS-Front-End-June-2024/06. DOM Manipulation and Events/Lab/02. Delete-From-Table/app.js
+++ JS-Front-End/JS-Front-End-June-2024/06. DOM Manipulation and Events/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,27 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  const emailInputElement = document.querySelector(
+    'input[type="text"][name="email"]'
+  );
+  const emailToDelete = document
+    .getElementsByName('email')[0]
+    .value.trim()
+    .toLowerCase();
+  const resultElement = document.getElementById('result');
+  resultElement.textContent = 'Not found.';
+
+  const rows = Array.from(document.querySelectorAll('tbody tr'));
+
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    const emailCellText = row
+      .querySelector('td:nth-child(2)')
+      .textContent.trim()
+      .toLowerCase();
+
+    if (emailCellText == emailToDelete) {
+      row.remove();
+      resultElement.textContent = 'Deleted.';
+      break;
+    }
+  }
 }

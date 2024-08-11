5411b8bd228256c922fe11818a5ec5245a24ef5f
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-08-11 12:03:17 +0300

    Upload

--- JS-Front-End/JS-Front-End-June-2024/06. DOM Manipulation and Events/Lab/05. Highlight-Active/app.js
+++ JS-Front-End/JS-Front-End-June-2024/06. DOM Manipulation and Events/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,15 @@
 function focused() {
-  let;
+  const textInputElements = document.querySelectorAll(
+    'div > input[type="text"]'
+  );
+
+  textInputElements.forEach((input) => {
+    input.addEventListener('focus', toggleFocus);
+    input.addEventListener('blur', toggleFocus);
+  });
+
+  function toggleFocus(event) {
+    const parentDiv = event.target.closest('div');
+    parentDiv?.classList.toggle('focused', event.type === 'focus');
+  }
 }

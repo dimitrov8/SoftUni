69e2ff5793793ab26a12414f0c04d41b8a7b7b35
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-08-11 14:48:13 +0300

    Upload

--- JS-Front-End/JS-Front-End-June-2024/06. DOM Manipulation and Events/Lab/07. Shopping-Cart/solution.js
+++ JS-Front-End/JS-Front-End-June-2024/06. DOM Manipulation and Events/Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,44 @@
 function solve() {
-   //TODO...
+  const buttonElements = document.querySelectorAll('.product .add-product');
+  const textArea = document.querySelector('textarea');
+  const checkOutElement = document.querySelector('.checkout');
+  let boughtProducts = {};
+
+  addEvents();
+
+  function addEvents() {
+    buttonElements.forEach((btnEl) => {
+      btnEl.addEventListener('click', addToCart);
+    });
+
+    checkOutElement.addEventListener('click', checkOut);
+  }
+
+  function addToCart(event) {
+    const productInfo = event.target.closest('.product');
+    const name = productInfo.querySelector('.product-title').textContent.trim();
+    const priceElement = productInfo.querySelector('.product-line-price');
+    const price = Number(priceElement.textContent);
+
+    if (boughtProducts[name]) {
+      boughtProducts[name] += price;
+    } else {
+      boughtProducts[name] = price;
+    }
+
+    textArea.value += `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
+  }
+
+  function checkOut(event) {
+    const boughtProductNames = Object.keys(boughtProducts).join(', ');
+
+    const totalPrice = Object.values(boughtProducts)
+      .reduce((sum, price) => sum + price, 0)
+      .toFixed(2);
+
+    buttonElements.forEach((btnEl) => (btnEl.disabled = true));
+    checkOutElement.disabled = true;
+
+    textArea.value += `You bought ${boughtProductNames} for ${totalPrice}.`;
+  }
 }
